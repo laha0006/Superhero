4b1de38d72d584e58da4b53aac25e22ec7daa0d4
Lars <laha0006@stud.kea.dk>
2023-09-11 22:39:11 +0200

    Created methods for menu and adding superhero to database. As well as main functionality for running the program.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,60 @@
 import java.util.Scanner;
 
 public class Main {
+    public static Input input = new Input();
+    public static Database DB = new Database();
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Input input = new Input(scanner);
 
-//        String test = input.inputString("Test: ", "Test skal skrives!");
-//        System.out.println(test);
-//        char test2 = input.inputChar("Char: ");
-//        System.out.println("test2 = " + test2);
-        int test3 = input.inputInt("Strength: ");
-        System.out.println("test3 = " + test3);
+        while(true) {
+            menu();
+        }
+
+
+    }
+
+    public static void menu() {
+        System.out.println("1. add superhero");
+        System.out.println("2. Show Database");
+        System.out.println("9. exit");
+        int choice = input.inputInt(": ");
+        switch(choice) {
+            case 1:
+                inputSuperhero();
+                break;
+            case 2:
+                System.out.println(DB);
+                break;
+            case 9:
+                System.exit(1);
+                break;
+            default:
+                System.exit(1);
+                break;
+        }
+    }
+
+    public static void inputSuperhero() {
+        boolean isHuman;
+        String name = input.inputString("Superhero name: ");
+        String realName = input.inputString("Real name: ");
+        String superPower = input.inputString("Superpower: ");
+        char isHumanYesNo = input.inputChar("Is Human? (y/n): ");
+        switch(isHumanYesNo) {
+            case 'y':
+                isHuman = true;
+                break;
+            case 'n':
+                isHuman = false;
+                break;
+            default:
+                isHuman = true;
+                break;
+        }
+        int strength = input.inputInt("Strength: ");
+        int yearCreated = input.inputInt("Year Created: ");
+
+        DB.addSuphero(name,realName,isHuman,superPower,yearCreated,strength);
+
 
     }
 }
