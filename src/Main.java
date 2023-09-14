414f02e0bed68ce6bfa5eacfa1ca9e67f9916589
Lars <laha0006@stud.kea.dk>
2023-09-14 20:59:54 +0200

    add searchMenu method to Main class.

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,53 @@
-import java.util.Scanner;
-
 public class Main {
     public static Input input = new Input();
     public static Database DB = new Database();
+
     public static void main(String[] args) {
 
-
-        while(true) {
-            menu();
+        boolean run = true;
+        while (run) {
+            run = menu();
         }
-
+        System.exit(0);
 
 
     }
 
-    public static void menu() {
+    public static boolean menu() {
         System.out.println("1. add superhero");
         System.out.println("2. Show Database");
+        System.out.println("3. Search Database");
         System.out.println("9. exit");
         int choice = input.inputInt(": ");
-        switch(choice) {
-            case 1:
+        return switch (choice) {
+            case 1 -> {
                 inputSuperhero();
-                break;
-            case 2:
+                yield true;
+            }
+            case 2 -> {
                 System.out.println(DB);
-                break;
-            case 9:
-                System.exit(1);
-                break;
-            default:
-                System.exit(1);
-                break;
+                yield true;
+            }
+            case 3 -> {
+                searchScreen();
+                yield true;
+            }
+            case 9 -> false;
+            default -> {
+                //System.exit(1337);
+                System.out.println("input number from the menu.");
+                yield true;
+            }
+        };
+    }
+
+    public static void searchScreen() {
+        String searchTerm = input.inputString("Search: ");
+        Superhero sh = DB.search(searchTerm);
+        if (sh != null) {
+            System.out.println(sh);
+        } else {
+            System.out.println("None found.");
         }
     }
 
@@ -41,7 +57,7 @@ public class Main {
         String realName = input.inputString("Real name: ");
         String superPower = input.inputString("Superpower: ");
         char isHumanYesNo = input.inputChar("Is Human? (y/n): ");
-        switch(isHumanYesNo) {
+        switch (isHumanYesNo) {
             case 'y':
                 isHuman = true;
                 break;
@@ -55,7 +71,7 @@ public class Main {
         int strength = input.inputInt("Strength: ");
         int yearCreated = input.inputInt("Year Created: ");
 
-        DB.addSuphero(name,realName,isHuman,superPower,yearCreated,strength);
+        DB.addSuperhero(new Superhero(name, realName, isHuman, superPower, yearCreated, strength));
 
 
     }
