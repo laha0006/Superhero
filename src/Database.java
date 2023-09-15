555c615d4006f34a5e9314d2a7595141d1e2e816
Lars <laha0006@stud.kea.dk>
2023-09-15 17:27:00 +0200

    Fixed database test data, to be accurate.

--- src/Database.java
+++ src/Database.java
@@ -5,11 +5,11 @@ public class Database {
     private final ArrayList<Superhero> superheroList;
     public Database() {
         superheroList = new ArrayList<>();
-        superheroList.add(new Superhero("Superman","Clark Kent",false,"Super.",999,1950));
-        superheroList.add(new Superhero("Batman","Bruce Wayne",true,"Bat",100,1940));
-        superheroList.add(new Superhero("Arrow","Joe Spencer",true,"Arrow guy",20,1950));
-        superheroList.add(new Superhero("Spiderman","Peter Parker",true,"Spider allergy",80,1930));
-        superheroList.add(new Superhero("Fat Fury","Herbie Popnecker",true,"Fat fury, what more to say",500,1958));
+        superheroList.add(new Superhero("Superman","Clark Kent",false,"Super.",1950,999));
+        superheroList.add(new Superhero("Batman","Bruce Wayne",true,"Bat",1940,100));
+        superheroList.add(new Superhero("Arrow","Joe Spencer",true,"Arrow guy",1950,100));
+        superheroList.add(new Superhero("Spiderman","Peter Parker",true,"Spider allergy",1930,88));
+        superheroList.add(new Superhero("Fat Fury","Herbie Popnecker",true,"Fat fury, what more to say",1958,200));
     }
     public void addSuperhero(Superhero superhero) {
         superheroList.add(superhero);
