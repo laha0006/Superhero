2c2698086f3a04f50d3e2403d7cb6907690452ce
Lars <laha0006@stud.kea.dk>
2023-09-15 09:12:27 +0200

    removed fields/properties/attributes from main.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
 public class Main {
-    public static Input input = new Input();
-    public static Database DB = new Database();
-
     public static void main(String[] args) {
         UserInterface userInterface = new UserInterface();
         userInterface.start();
