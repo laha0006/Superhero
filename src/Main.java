ad4b79d8b0dbaeb5da6687c6db50b719b5f85d30
Lars <laha0006@stud.kea.dk>
2023-09-15 21:26:39 +0200

    main changed for testing purposes.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
-        UserInterface userInterface = new UserInterface();
-        userInterface.start();
+    public static void main(String[] args) throws Exception {
+//        UserInterface userInterface = new UserInterface();
+//        userInterface.start();
+        String header = "Header";
+        ArrayList<String> columns = new ArrayList<>();
+        columns.add("Superhero");
+        columns.add("Name");
+        columns.add("Human");
+        columns.add("Superpower");
+        columns.add("Strength");
+        columns.add("Year Created");
+        Table table = new Table(header, columns);
+        table.addRow(new Row()
+                .addCell("Superman")
+                .addCell("Clark Kent")
+                .addCell(false)
+                .addCell("Super strength.")
+                .addCell(9000)
+                .addCell(1954));
+
+        table.print();
+
     }
 }
