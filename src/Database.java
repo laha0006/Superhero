2b7711ea6a00ee5066aa8d36680b5310b685df9e
Lars <laha0006@stud.kea.dk>
2023-09-15 12:05:40 +0200

    small refactor. Added test objects to Database.

--- src/Database.java
+++ src/Database.java
@@ -1,8 +1,16 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Database {
-    private final ArrayList<Superhero> superheroList = new ArrayList<>();
-    public Database() {}
+    private final ArrayList<Superhero> superheroList;
+    public Database() {
+        superheroList = new ArrayList<>();
+        superheroList.add(new Superhero("Superman","Clark Kent",false,"Super.",999,1950));
+        superheroList.add(new Superhero("Batman","Bruce Wayne",true,"Bat",100,1940));
+        superheroList.add(new Superhero("Arrow","Joe Spencer",true,"Arrow guy",20,1950));
+        superheroList.add(new Superhero("Spiderman","Peter Parker",true,"Spider allergy",80,1930));
+        superheroList.add(new Superhero("Fat Fury","Herbie Popnecker",true,"Fat fury, what more to say",500,1958));
+    }
     public void addSuperhero(Superhero superhero) {
         superheroList.add(superhero);
     }
@@ -13,8 +21,18 @@ public class Database {
                 return superhero;
             }
         }
-
         return null;
+    }
+
+    public ArrayList<Superhero> searchMany(String superheroName) {
+        ArrayList<Superhero> superheroes = new ArrayList<>();
+        for(Superhero superhero : superheroList) {
+            if(superhero.getName().contains(superheroName)) {
+                System.out.println("Found!");
+                superheroes.add(superhero);
+            }
+        }
+        return superheroes;
     }
 
 
