0706110390b1249e0d4b1d56c504f85f83b50384
Lars <laha0006@stud.kea.dk>
2023-09-16 02:24:41 +0200

    added another table to test

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 //        UserInterface userInterface = new UserInterface();
 //        userInterface.start();
+
+
+
+
         String header = "HeaderHere";
         ArrayList<String> columns = new ArrayList<>();
-        columns.add("Superhero");
+        columns.add("Superheroherohero");
         columns.add("Name");
         columns.add("Human");
         columns.add("Superpower");
@@ -22,8 +27,18 @@ public class Main {
                 .addCell(1954));
 
         table.print();
+        System.out.println();
+        Table table2 = new Table("TFT Tier List", new ArrayList<>(List.of("Tier",
+                "Synergy", "Difficulity")));
+        table2.addRow(new Row()
+                .addCell("S")
+                .addCell("Blightwater")
+                .addCell("0"));
 
-
-
+        table2.addRow(new Row()
+                .addCell("A")
+                .addCell("Azir")
+                .addCell("3"));
+        table2.print();
     }
 }
