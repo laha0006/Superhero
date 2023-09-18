ede88d50d0660570982a60310f3ca7b6eff9f707
Lars <laha0006@stud.kea.dk>
2023-09-18 18:12:00 +0200

    comments

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
 //        UserInterface userInterface = new UserInterface();
 //        userInterface.start();
+        //testing.
         Screen mainScreen = new MainMenu("NameHereLol");
         mainScreen.show();
 
