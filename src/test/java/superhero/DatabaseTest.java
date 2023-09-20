8eec36549c302e784a345f2001a646ed6a3ce884
Lars <laha0006@stud.kea.dk>
2023-09-20 09:36:03 +0200

    made test for delete method (yet to be implemented)

--- src/test/java/superhero/DatabaseTest.java
+++ src/test/java/superhero/DatabaseTest.java
@@ -1,5 +1,7 @@
 package superhero;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -13,6 +15,20 @@ class DatabaseTest {
     public DatabaseTest(){
         db = new Database();
     }
+
+//    @BeforeEach
+//    void setup() {
+//        db.addSuperhero(new Superhero("Superman", "Clark Kent", false, "Super.", 1950, 999));
+//        db.addSuperhero(new Superhero("Batman", "Bruce Wayne", true, "Bat", 1940, 100));
+//        db.addSuperhero(new Superhero("Arrow", "Joe Spencer", true, "Arrow guy", 1950, 100));
+//        db.addSuperhero(new Superhero("Spiderman", "Peter Parker", true, "Spider allergy", 1930, 88));
+//        db.addSuperhero(new Superhero("Fat Fury", "Herbie Popnecker", true, "Fat fury, what more to say", 1958, 200));
+//    }
+
+//    @AfterEach
+//    void teardown() {
+//        db.deleteAll();
+//    }
 
     @Test
     void testSearch() {
@@ -31,16 +47,21 @@ class DatabaseTest {
     }
     @Test
     void testSearchMany() {
-        //superheroList.add(new Superhero("Superman", "Clark Kent", false, "Super.", 1950, 999));
-        //superheroList.add(new Superhero("Batman", "Bruce Wayne", true, "Bat", 1940, 100));
-        //superheroList.add(new Superhero("Arrow", "Joe Spencer", true, "Arrow guy", 1950, 100));
-        //superheroList.add(new Superhero("Spiderman", "Peter Parker", true, "Spider allergy", 1930, 88));
-        //superheroList.add(new Superhero("Fat Fury", "Herbie Popnecker", true, "Fat fury, what more to say", 1958, 200));
         ArrayList<Superhero> heroList1 = db.searchMany("man");
         ArrayList<Superhero> heroList2 = db.searchMany("mAn");
         assertAll("Should return ArrayList<Superhero> of size 3",
                 ()-> assertEquals(3,heroList1.size()),
                 () -> assertEquals(3,heroList2.size()));
 
+    }
+
+    @Test
+    void testDelete() {
+        //delete by index
+        db.delete(0);
+        assertAll("Deleting index 0(Superman), should make size 4,and the first object Batman",
+                () -> assertEquals(4,db.getSuperheroList().size()),
+                () -> assertEquals("Batman",db.getSuperheroList().get(0).getName()));
+        //fail();
     }
 }
